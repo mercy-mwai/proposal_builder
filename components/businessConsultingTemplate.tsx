3f4d4cfa30ab63f84df377410fe724c51eff6aa7
mercy-mwai <mercyneemam@gmail.com>
2025-09-19 12:40:48 +0300

    new file:   components/businessConsultingTemplate.tsx

---
+++ components/businessConsultingTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BusinessConsultingTemplate = () => {
+  return (
+    <div>business-consulting-template</div>
+  );
+}
+
+export default BusinessConsultingTemplate;
